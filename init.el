067402862a3aff9813dbc795f823463f68ad98af
pat-thomas <patthomassoftware@gmail.com>
2013-10-15 18:09:45 -0400

    basic package installation

--- init.el
+++ init.el
@@ -1,1 +1,14 @@
+;; --- Bring in Marmalade for package management.
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" .
+               "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; --- Packages to install.
+;; (package-install 'clojure-mode)
+;; (package-install 'paredit)
+;; (package-install 'nrepl)
+
+;; --- User specific settings.
 (setq-default tab-width 2)
