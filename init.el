a4cbaef9557fb4992152fb0852181c2c8c3f366c
pat-thomas <patthomassoftware@gmail.com>
2013-10-15 18:36:16 -0400

    visual tweaks

--- init.el
+++ init.el
@@ -1,15 +1,25 @@
-;; --- Bring in Marmalade for package management.
+;; --- Bring in Marmalade for package installation.
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" .
                "http://marmalade-repo.org/packages/"))
+;; --- Bring in MELPA for package installation.
+(add-to-list 'package-archives
+						 '("melpa" .
+							 "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
+
 
 ;; --- Packages to install.
 ;; (package-install 'clojure-mode)
 ;; (package-install 'paredit)
 ;; (package-install 'nrepl)
+;; (package-install 'cyberpunk-theme)
+
 
 ;; --- User specific settings.
 (setq-default tab-width 2)
 (setq inhibit-splash-screen t)
+(load-theme 'cyberpunk t)
+(menu-bar-mode -1)
+
