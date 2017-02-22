0ffe53f3b47f6432ab7ad1b11bb4468c6315e148
pat-thomas <patthomassoftware@gmail.com>
2017-02-22 16:55:41 -0500

    More customizations.

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 	(add-to-list 'package-archives
 							 '("melpa" .
 								 "http://melpa.milkbox.net/packages/") t)
-	(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+	;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 	(package-initialize))
 
 ;; --- Packages to install.
@@ -20,6 +20,7 @@
                            cider
                            clj-refactor
 													 clojure-mode
+                           color-theme-sanityinc-tomorrow
 													 company
 													 cyberpunk-theme
 													 erlang
@@ -64,7 +65,7 @@
 	(setq javascript-indent-level 2)
 	(show-paren-mode 1)
 	(global-hl-line-mode 1)
-	(global-aggressive-indent-mode 1)
+	;;(global-aggressive-indent-mode 1)
 	(set-face-background hl-line-face "black20"))
 
 (defun load-ido-mode ()
@@ -234,3 +235,48 @@
 (load-tidal)
 (load-rust-mode-customizations)
 (load-company-mode-customizations)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+	 [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+	 (vector "#ffffff" "#ff9da4" "#d1f1a9" "#ffeead" "#bbdaff" "#ebbbff" "#99ffff" "#003f8e"))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-safe-themes
+	 (quote
+		("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default)))
+ '(fci-rule-color "#003f8e")
+ '(package-selected-packages
+	 (quote
+		(color-theme-sanityinc-tomorrow ujelly-theme tagedit smex rust-mode rainbow-delimiters haskell-mode go-mode erlang elixir-mode cyberpunk-theme company clj-refactor auto-complete align-cljlet aggressive-indent)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#ff9da4")
+		 (40 . "#ffc58f")
+		 (60 . "#ffeead")
+		 (80 . "#d1f1a9")
+		 (100 . "#99ffff")
+		 (120 . "#bbdaff")
+		 (140 . "#ebbbff")
+		 (160 . "#ff9da4")
+		 (180 . "#ffc58f")
+		 (200 . "#ffeead")
+		 (220 . "#d1f1a9")
+		 (240 . "#99ffff")
+		 (260 . "#bbdaff")
+		 (280 . "#ebbbff")
+		 (300 . "#ff9da4")
+		 (320 . "#ffc58f")
+		 (340 . "#ffeead")
+		 (360 . "#d1f1a9"))))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
